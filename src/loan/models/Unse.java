25c58d80f2434c42837ccdae7b4c1e385cb1f1ba
Samman <sammannsingh.031005@gmail.com>
2025-09-08 22:29:35 +0530

    compiled and sent to bin for running

--- src/loan/models/Unse.java
+++
@@ -1,9 +0,0 @@
-package loan.models;
-
-import loan.core.Repayment;
-import loan.utils.IDGenerator;
-
-import java.time.LocalDate;
-import java.util.Scanner;
-
-public class UnsecuredLoan extends Loan{}
